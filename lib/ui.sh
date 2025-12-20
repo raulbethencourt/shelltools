f457075680c1d87c2ae054308a09f77f96f3aabf
raulbethencourt <raul.bethencourt.pro@gmail.com>
2025-12-20 22:23:53 +0100

    fix(AnsiColors): remove function and transform into constants

--- lib/ui.sh
+++ lib/ui.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
 
-initANSI() {
-  # Foreground colors
-  blackf=$(tput setaf 0)
-  redf=$(tput setaf 1)
-  greenf=$(tput setaf 2)
-  yellowf=$(tput setaf 3)
-  bluef=$(tput setaf 4)
-  purplef=$(tput setaf 5)
-  cyanf=$(tput setaf 6)
-  whitef=$(tput setaf 7)
+# ANSI Escape Sequences - Computed once at source time
+readonly BLACKF="$(tput setaf 0)"
+readonly REDF="$(tput setaf 1)"
+readonly GREENF="$(tput setaf 2)"
+readonly YELLOWF="$(tput setaf 3)"
+readonly BLUEF="$(tput setaf 4)"
+readonly PURPLEF="$(tput setaf 5)"
+readonly CYANF="$(tput setaf 6)"
+readonly WHITEF="$(tput setaf 7)"
 
-  # Background colors
-  blackb=$(tput setab 0)
-  redb=$(tput setab 1)
-  greenb=$(tput setab 2)
-  yellowb=$(tput setab 3)
-  blueb=$(tput setab 4)
-  purpleb=$(tput setab 5)
-  cyanb=$(tput setab 6)
-  whiteb=$(tput setab 7)
+readonly BLACKB="$(tput setab 0)"
+readonly REDB="$(tput setab 1)"
+readonly GREENB="$(tput setab 2)"
+readonly YELLOWB="$(tput setab 3)"
+readonly BLUEB="$(tput setab 4)"
+readonly PURPLEB="$(tput setab 5)"
+readonly CYANB="$(tput setab 6)"
+readonly WHITEB="$(tput setab 7)"
 
-  # Bold, italic, underline and inverse style toggles
-  boldon=$(tput smso)
-  boldoff=$(tput rmso)
-  italicon=$(tput sitm)
-  italicoff=$(tput ritm)
-  ulon=$(tput smul)
-  uloff=$(tput rmul)
-  inver=$(tput rev)
+readonly BOLDON="$(tput smso)"
+readonly BOLDOFF="$(tput rmso)"
+readonly ITALICON="$(tput sitm)"
+readonly ITALICOFF="$(tput ritm)"
+readonly ULON="$(tput smul)"
+readonly ULOFF="$(tput rmul)"
+readonly INVER="$(tput rev)"
 
-  reset=$(tput sgr0)
-}
+readonly RESET="$(tput sgr0)"
 
 print_progress() {
   BAR_FULLSIZE=${3:-28}
