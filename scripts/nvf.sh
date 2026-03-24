a2dcf241669a8152eeb6926c1ed2abe20ee7a07b
raul-bns <raul.bethencourt@bluenot-systems.com>
2026-03-24 21:47:22 +0100

    fix(nvf): better fzf use

--- scripts/nvf.sh
+++ scripts/nvf.sh
@@ -5,9 +5,13 @@
 
 searchr=$(
   fd --type f --hidden --exclude .git . "$HOME/" |
-    fzf --tmux center,70%,60% -i --bind=tab:up --bind=btab:down \
+    fzf --tmux center,80%,70% -i --bind=tab:up --bind=btab:down \
+      --padding 0 --margin 0 \
       --bind=ctrl-g:first --style full --info=inline \
+      --bind=ctrl-d:preview-half-page-down --bind=ctrl-u:preview-half-page-up \
       --prompt '󰀘  ' --info=hidden \
+      --preview 'bat --style=full --color=always {}' \
+      --preview-window 'right,55%' \
       --color 'prompt:#ea6962' \
       --color 'border:#414b50' \
       --color 'preview-border:#414b50' \
